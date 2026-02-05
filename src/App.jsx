3c35d10f716e11fc067c27dde07778a8480b7dd0
rubendario <rubendario.inlo@gmail.com>
2026-02-05 13:59:17 +0100

    cambio react por ruben

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + ruben</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          dario<code>src/App.jsx</code> and save to test HMR
+          <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
