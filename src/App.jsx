fbe1081d4ba1817bda1e6425d939f3194f3e8b28
rubendario <rubendario.inlo@gmail.com>
2026-02-05 13:56:26 +0100

    cambio nombre a dario

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          ruben<code>src/App.jsx</code> and save to test HMR
+          dario<code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
