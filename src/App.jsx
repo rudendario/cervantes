f161bd9a1dbb9709133df17aef6e2108754bdfdc
rubendario <rubendario.inlo@gmail.com>
2026-02-05 14:13:00 +0100

    modification of App.jsx and Route.jsx to test HMR

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,7 @@ function App() {
   return (
     <>
       <Header />
-      <Route path="/" component={HomePage} />
-      <Route path="/search" component={SearchPage} />
+
       <Footer />
     </>
   )
